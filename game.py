2e5b92c34e2c70f414526807647d66177856f165
BPChance <bradenpchance6@gmail.com>
2024-08-08 16:02:42 -0500

    character sizing

--- game.py
+++ game.py
@@ -7,11 +7,27 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+sprite_sheet = pygame.image.load('sprites/characters/player.png').convert_alpha()
+
+frame_width = 64
+frame_height = 64
+
+def get_frame(sprite_sheet, frame_rect):
+    return sprite_sheet.subsurface(frame_rect).copy()
+
+#extract first frame
+frame_rect = pygame.Rect(0, 0, frame_width, frame_height)
+character_frame = get_frame(sprite_sheet, frame_rect)
+
+#character size
+scale_factor = 2
+
+character_frame = pygame.transform.scale(character_frame, (frame_width * scale_factor, frame_height * scale_factor))
+
 world_size = pygame.Vector2(2000, 2000)
 world_rect = pygame.Rect(0, 0, world_size.x, world_size.y)
 
 player_pos = pygame.Vector2(screen_size.get_width() / 2, screen_size.get_height() / 2)
-
 camera = pygame.Vector2(0, 0)
 
 threshhold_x = 200
@@ -24,7 +40,6 @@ while running:
 
     screen_size.fill("purple")
 
-    pygame.draw.circle(screen_size, "red", player_pos, 40)
 
     # Moves the player
     keys = pygame.key.get_pressed()
@@ -51,6 +66,7 @@ while running:
         camera.y = min(world_size.y - screen_size.get_height(), player_pos.y - (screen_size.get_height() - threshhold_y))
     
     player_screen_pos = player_pos - camera
+    screen_size.blit(character_frame, player_screen_pos)
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
